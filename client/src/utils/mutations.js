441398d25a0762d564571731e968532240575935
Abby Castelow <abby.castelow@gmail.com>
2021-10-08 02:33:20 -0600

    changes to REMOVE_BOOK gql

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -43,19 +43,10 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
       _id
       username
-      email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
     }
   }
 `;
